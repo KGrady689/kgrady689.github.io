85f1b9cda2f7487b9e1b3c4e383d775683f89812
KGrady689 <93741269+KGrady689@users.noreply.github.com>
2022-10-01 11:15:40 -0400

    Update fizzbuzz0.js

--- index/web115/greatdistance/fizzbuzz0.js
+++ index/web115/greatdistance/fizzbuzz0.js
@@ -3,5 +3,5 @@
 /* SWTL JS (Great Distance */
 
 function greetUser() {
-  print('Welcome to the Future, ' + document.getElementById("firstName") + ' ' + document.getElementById("midIn") + '. ' + document.getElementById("lastName") + '.')
+  console.log('Welcome to the Future, ' + document.getElementById("firstName") + ' ' + document.getElementById("midIn") + '. ' + document.getElementById("lastName") + '.')
 }
