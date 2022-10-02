738f344c9efa10590d068598848cd79927bb952b
KGrady689 <93741269+KGrady689@users.noreply.github.com>
2022-10-02 12:10:44 -0400

    Update fizzbuzz.js

--- index/web115/greatdistance/fizzbuzz.js
+++ index/web115/greatdistance/fizzbuzz.js
@@ -4,8 +4,9 @@
 function greetUser() {
   var firstName = document.getElementById("firstName").value; 
   var midIn = document.getElementById("midIn").value; 
-  var lastName = document.getElementById("lastName").value; 
-  
+  var lastName = document.getElementById("lastName").value;
   alert("Greetings, " + firstName + " " + midIn + ". " + lastName + ".");
-  document.getElementById("joinUs").innerHTMl = "Welcome to the Great Distance Family, " + firstName + "!";
+  
+  var joinUs = document.getElementById("joinUs").innerHTMl;
+  joinUs.textContent = "Welcome to the Great Distance Family, " + firstName + "!";
 }
