17ad70ff526c11e1a0e30cc52df23a54ef326f18
KGrady689 <93741269+KGrady689@users.noreply.github.com>
2022-10-02 16:15:24 -0400

    Update fizzbuzz.js

--- index/web115/greatdistance/fizzbuzz.js
+++ index/web115/greatdistance/fizzbuzz.js
@@ -6,6 +6,13 @@ function greetUser() {
   var midIn = document.getElementById("midIn").value; 
   var lastName = document.getElementById("lastName").value;
   alert("Greetings, " + firstName + " " + midIn + ". " + lastName + ".");
+  var userNum = prompt("How many years into the future do you want to go, " + firstName + "?");
+			for (var currentNum = 0; currentNum <= userNum; currentNum++) {
+			If (userNum % 2 == 0) {
+				document.getElementById("countUp").innerHTML = userNum + "). " + "Your number is Even!" +  "<br> " + "<br>";
+			} else {
+				document.getElementById("countUp").innerHTML = userNum + "). " + "Your number is Odd!" +  "<br> " + "<br>";
+			}
   
   var joinUs = document.getElementById("joinUs").innerHTML;
   joinUs.textContent = "Welcome to the Great Distance Family, " + firstName + "!";
