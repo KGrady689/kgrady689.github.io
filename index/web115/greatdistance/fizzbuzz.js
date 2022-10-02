f8488f664883bb3529332a4e560db9c762f38d2e
KGrady689 <93741269+KGrady689@users.noreply.github.com>
2022-10-02 11:33:21 -0400

    Update fizzbuzz.js

--- index/web115/greatdistance/fizzbuzz.js
+++ index/web115/greatdistance/fizzbuzz.js
@@ -2,9 +2,9 @@
 /* SWTL JS (Great Distance) */
 
 function greetUser() {
-  var firstName = document.getElementById("firstName"); 
-  var midIn = document.getElementById("midIn"); 
-  var lastName = document.getElementById("lastName"); 
+  var firstName = document.getElementById("firstName").value; 
+  var midIn = document.getElementById("midIn").value; 
+  var lastName = document.getElementById("lastName").value; 
   
   alert("Greetings, " + firstName + " " + midIn + ". " + lastName + ".")
 }
