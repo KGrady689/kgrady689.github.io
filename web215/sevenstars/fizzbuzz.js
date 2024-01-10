9ee60e6469cd35bacb37b6bd06cc72a2907dfaf0
KGrady689 <93741269+KGrady689@users.noreply.github.com>
2024-01-09 21:10:53 -0500

    Create fizzbuzz.js

---
+++ web215/sevenstars/fizzbuzz.js
@@ -0,0 +1,30 @@
+
+/* SWTL JS (SEVEN ✦ STARS) */
+
+function greetUser() {
+  var firstName = document.getElementById("firstName").value; 
+  var midIn = document.getElementById("midIn").value; 
+  var lastName = document.getElementById("lastName").value;
+  alert("Greetings, " + firstName + " " + midIn + ". " + lastName + ".");
+
+function askNumber() {
+	var userNum = prompt("How many years into the future do you want to go, " + firstName + "?");
+	for (var currentNum = 0; currentNum <= userNum; currentNum++) {
+		If (userNum % 2 == 0) {
+			document.getElementById("countUp").innerHTML = userNum + "). " + "Your number is Even!" +  "<br> " + "<br>";
+		} else {
+			document.getElementById("countUp").innerHTML = userNum + "). " + "Your number is Odd!" +  "<br> " + "<br>";
+		}
+	}
+  
+  var joinUs = document.getElementById("joinUs").innerHTML;
+  joinUs.textContent = "Welcome to the SEVEN ✦ STARS Family, " + firstName + "!";
+}
+	
+if (currentNum % 3 == 0 && currentNum % 5 = 0) {
+					document.getElementById("countUp").innerHTML += currentNum + "). " + "Super Eureka!" + "<br> " + "<br>";
+				} else if (currentNum % 3 == 0) {
+					document.getElementById("countUp").innerHTML += currentNum + "). " + "Eureka!" + "<br> " + "<br>";
+				} else {
+					document.getElementById("countUp").innerHTML += currentNum + "). " + "Think..." + "<br> " + "<br>";
+				}
